cfdfd7df89835867acaee1ed32f89387e4884e62
Alexander Joseph <alexander.v.joseph@gmail.com>
2017-09-21 11:21:43 -0600

    updated user model

--- business_management/accounts/models.py
+++ business_management/accounts/models.py
@@ -50,10 +50,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     digital_sig = models.ImageField(blank=True, null=True)
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(null=True)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    email_confirmed = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
+    email_confirmed = models.BooleanField(default=True)
     
     objects = UserManager()
     
