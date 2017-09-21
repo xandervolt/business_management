b73a2964c70ba773dc0f4da03781359fb09a9af3
Alexander Joseph <alexander.v.joseph@gmail.com>
2017-09-20 20:45:13 -0600

    changed prod settings file to add new host

--- config/settings/production.py
+++ config/settings/production.py
@@ -33,4 +33,4 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cloud.digitalocean.com', 'digitalocean.com', 'corporate.optipulse.com', 'optipulse.com', 'localhost', '138.68.48.73',]
+ALLOWED_HOSTS = ['127.0.0.1', 'cloud.digitalocean.com', 'digitalocean.com', 'corporate.optipulse.com', 'optipulse.com', 'localhost', '138.68.48.73', '138.68.57.75',]
