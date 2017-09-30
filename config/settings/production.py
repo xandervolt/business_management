5a72001775ac87c35a761b3e4f02b5b95b6d12d5
Alexander Joseph <alexander.v.joseph@gmail.com>
2017-09-30 09:19:47 -0600

    changed staticfiles config for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -28,7 +28,7 @@ INSTALLED_APPS += ['gunicorn', ]
 STATIC_ROOT = str(ROOT_DIR('static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/static/"
+STATIC_URL = "https://corporate.optipulse.com/static/"
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
