4868e9e6b1a48f1dd1d833ce407653d5f263be2b
Alexander Joseph <alexander.v.joseph@gmail.com>
2017-09-30 09:08:04 -0600

    changed staticfiles config for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -25,10 +25,10 @@ INSTALLED_APPS += ['gunicorn', ]
 # Static File Configuration
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_ROOT = str(ROOT_DIR('static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
